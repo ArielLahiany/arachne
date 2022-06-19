cc016078710540742e7e1519790cb8af0b1bc49f
ArielLahiany <ariel.lahiany@gmail.com>
2022-06-19 21:37:35 +0300

    Adding basic HTTP server logic.

--- server.go
+++ server.go
@@ -1,9 +1,45 @@
 package main
 
 import (
+	"flag"
 	"fmt"
+	"log"
+	"net/http"
 )
 
+var (
+	addr = flag.String(
+		"addr",
+		"0.0.0.0",
+		"Listening address of the server on the",
+	)
+	port = flag.Int(
+		"port",
+		8080,
+		"Listening port of the server",
+	)
+)
+
+func rootHandler(w http.ResponseWriter, r *http.Request) {
+	log.Printf("Hello, world!")
+}
+
 func main() {
-	fmt.Println("Hello, world!")
+	flag.Parse()
+	log.Printf("Listening and serving on %s:%v",
+		*addr,
+		*port,
+	)
+	http.HandleFunc("/", rootHandler)
+	err := http.ListenAndServe(
+		fmt.Sprintf(
+			"%s:%v",
+			*addr,
+			*port,
+		),
+		nil,
+	)
+	if err != nil {
+		log.Fatal(err)
+	}
 }
