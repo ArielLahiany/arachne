8f594449bdffa5386979f04a718d84b6433c541b
ArielLahiany <ariel.lahiany@gmail.com>
2022-06-20 07:43:37 +0300

    Adding basic database logic.

---
+++ sql/00001_attack.sql
@@ -0,0 +1,6 @@
+CREATE TABLE IF NOT EXISTS attack (
+    id          SERIAL          PRIMARY KEY,
+    created_at  TIMESTAMP       NOT NULL        DEFAULT NOW(),
+    updated_at  TIMESTAMP       NOT NULL        DEFAULT NOW(),
+    target      VARCHAR(2048)   NOT NULL
+);
