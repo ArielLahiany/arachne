6afc95414e1bd1bd00380b0e3bcc2b8efca42253
ArielLahiany <ariel.lahiany@gmail.com>
2022-06-20 08:51:00 +0300

    Adding timestampes.

--- pkg/model/attack.go
+++ pkg/model/attack.go
@@ -1,8 +1,14 @@
 package model
 
+import (
+	"time"
+)
+
 type Attack struct {
-	ID		int64	`db:"id" json:"id"`
-	Target	string	`db:"target" json:"target"`
+	ID			int64		`db:"id" json:"id"`
+	CreatedAt	time.Time 	`db:"created_at" json:"createdAt"`
+	UpdatedAt 	time.Time 	`db:"updated_at" json:"updatedAt"`
+	Target		string		`db:"target" json:"target"`
 }
 
 type AttackResponse struct {
