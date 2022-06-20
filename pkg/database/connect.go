2bd665c0c8a23022e6c68946db8688d649c9edaa
ArielLahiany <ariel.lahiany@gmail.com>
2022-06-20 08:59:35 +0300

    Adding basic database connection logic.

--- pkg/database/connect.go
+++ pkg/database/connect.go
@@ -1,1 +1,58 @@
 package database
+
+import (
+	"context"
+	"fmt"
+	"log"
+	"os"
+
+	"github.com/jackc/pgx/v4/pgxpool"
+)
+
+type Locator struct {
+	User     string
+	Password string
+	Hostname string
+	Port     string
+}
+
+func NewLocator() *Locator {
+	environment := os.Getenv("ENVIRONMENT")
+	if environment == "PRODUCTION" {
+		return &Locator{
+			User:     os.Getenv("POSTGRES_USER"),
+			Password: os.Getenv("POSTGRES_PASS"),
+			Hostname: os.Getenv("POSTGRES_HOSTNAME"),
+			Port:     os.Getenv("POSTGRES_PORT"),
+		}
+	}
+
+	return &Locator{
+		User:     "arachne",
+		Password: "arachne",
+		Hostname: "localhost",
+		Port:     "5432",
+	}
+}
+
+func Connect() *pgxpool.Pool {
+	locator := NewLocator()
+	connection, err := pgxpool.Connect(
+		context.Background(),
+		fmt.Sprintf(
+			"postgres://%s:%s@%s:%s/postgres",
+			locator.User,
+			locator.Password,
+			locator.Hostname,
+			locator.Port,
+		),
+	)
+	if err != nil {
+		log.Printf(
+			"Unable to connect to PostgreSQL: %v",
+			err,
+		)
+	}
+
+	return connection
+}
